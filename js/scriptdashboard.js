9dc83c83c79fc9957f574f38ae145bf6a467bf30
Phanuel <ndikumanaphanuel@gmail.com>
2023-01-04 14:46:26 +0200

    some changes

--- js/scriptdashboard.js
+++ js/scriptdashboard.js
@@ -1,17 +1,20 @@
-const menusOnLeft = document.querySelector("body-divs");
+const menusOnLeft = document.querySelector("side-nav");
 window.addEventListener ("scroll", function() {
 	menusOnLeft.classList.toggle ("sticky", window.scrollY > 100);
 });
 
 let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let navlist = document.querySelector('.side-title');
+// let topNav = document.querySelector('.top-nav');
 
 menu.onclick = () => {
 	menu.classList.toggle('bx-x');
 	navlist.classList.toggle('open');
+// topNav.classList.toggle('open');
 };
 
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navlist.classList.remove('open');
+// topNav.classList.remove('open');
 };
