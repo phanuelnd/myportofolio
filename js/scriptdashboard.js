e88f50c88d37a4b3f30bcfbc35996ba71ca54010
Phanuel <ndikumanaphanuel@gmail.com>
2023-01-05 19:09:13 +0200

    after putting some firebase

--- js/scriptdashboard.js
+++ js/scriptdashboard.js
@@ -1,18 +1,18 @@
-const header = document.querySelector("header");
+const header = document.querySelector(".sidenav");
 window.addEventListener("scroll", function() {
 	header.classList.toggle("sticky", window.scrollY > 100);
 });
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navList');
+let menu = document.getElementById('menu-icon');
+let navlist1 = document.querySelector('.navList');
 
 menu.onclick = () => {
 	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('open');
+	navlist1.classList.toggle('open');
 };
 
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
-	navlist.classList.remove('open');
+	navlist1.classList.remove('open');
 };
 
